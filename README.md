152d310751054deadbe49b0fbf9d357b2bf996d7
Julien Landuré <julien.landure@gmail.com>
2020-06-03 14:59:53 +0200

    :sparkles: With playwright (#85) :tada:
    * :pencil2: update version
    
    * :sparkles: test with Playwright
    
    * :globe_with_meridians: add the support of asian characters by default :muscle:
    
    * :sparkles: add webgl libraries by default :muscle:
    
    * :arrow_up: Update dependency puppeteer to v3 (#54)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * :arrow_up: update puppeteer to 3.0.4
    
    * :pencil2: update chromium version and size
    
    * :sparkles: play with deno 🦕 (#89)
    
    * :sparkles: play with deno 🦕
    
    * :pencil2: end of file
    
    * :busts_in_silhouette: docs: add piotr-sikora-v as a contributor (#91)
    
    * docs: update README.md [skip ci]
    
    * docs: update .all-contributorsrc [skip ci]
    
    Co-authored-by: allcontributors[bot] <46447321+allcontributors[bot]@users.noreply.github.com>
    
    * :pencil2: update chromium version and size
    
    * :pencil2: update readme
    
    * :pencil2: Fix typos (#93)
    
    * :busts_in_silhouette: docs: add pgrimaud as a contributor (#94)
    
    * docs: update README.md [skip ci]
    
    * docs: update .all-contributorsrc [skip ci]
    
    Co-authored-by: allcontributors[bot] <46447321+allcontributors[bot]@users.noreply.github.com>
    
    * :arrow_up: update Chrome 83
    
    * :sparkles: first version of with-playwright working! :tada:
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: allcontributors[bot] <46447321+allcontributors[bot]@users.noreply.github.com>
    Co-authored-by: Pierre Grimaud <grimaud.pierre@gmail.com>

--- README.md
+++ README.md
@@ -157,6 +157,18 @@ These websites are tested with the following supported languages:
 - Japanese (with `https://www.yahoo.co.jp/`)
 - Korean (with `https://www.naver.com/`)
 
+# How to use with Playwright
+
+Like ["Puppeteer"](https://pptr.dev/#?product=Puppeteer&version=v1.15.0&show=api-class-browser), we can do a lot things using ["Playwright"](https://playwright.dev/#version=v1.0.1&path=docs%2Fcore-concepts.md&q=browser) with our Chrome Headless.
+
+Go to the `with-playwright` folder and launch the following command:
+
+```
+docker container run -it --rm -v $(pwd):/usr/src/app --cap-add=SYS_ADMIN zenika/alpine-chrome:with-playwright npm start
+```
+
+A `example-chromium.png` file will be created in your `with-playwright` folder.
+
 # How to use with WebGL
 
 By default, this image works with WebGL.
---
+++ with-playwright/src/useragent.js
@@ -0,0 +1,26 @@
+const { chromium } = require("playwright-chromium");
+
+(async () => {
+  const { exec } = require("child_process");
+  exec(
+    process.env.PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH + " --version",
+    function callback(error, stdout, stderr) {
+      console.log(stdout);
+    }
+  );
+})();
+(async () => {
+  const browser = await chromium.launch({
+    executablePath: process.env.PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH,
+    // 💡 This enables logs for the communication between Playwright and Chromium
+    // logger: {
+    //   isEnabled: (name, severity) => name === "browser" || "context",
+    //   log: (name, severity, message, args) => console.log(`${name} ${message}`),
+    // },
+  });
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto("http://whatsmyuseragent.org/");
+  await page.screenshot({ path: `example-chromium.png` });
+  await browser.close();
+})();
