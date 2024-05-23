7827cadc1188f7c0c0bee633ef42af2ce39293df
Alexandre Garnier <alexandre.garnier@zenika.com>
2024-05-23 11:23:35 +0200

    ✅ Improve tests scripts
    - Add output to distinguish common and specific tests
    - Set image to use right one in common tests when running locally

--- with-deno/test.sh
+++ with-deno/test.sh
@@ -1,5 +1,10 @@
 #!/bin/sh -e
 
+IMAGE_NAME=${IMAGE_NAME:-zenika/alpine-chrome:with-deno}
+export IMAGE_NAME
+
 DIR=$(cd $(dirname $0) && pwd)
 ${DIR}/../test.sh
-docker container run --rm --entrypoint '' ${IMAGE_NAME:-zenika/alpine-chrome:with-deno} deno --version
+
+echo "# Deno test"
+docker container run --rm --entrypoint '' ${IMAGE_NAME} deno --version
--- with-selenoid/test.sh
+++ with-selenoid/test.sh
@@ -1,5 +1,10 @@
 #!/bin/sh -e
 
+IMAGE_NAME=${IMAGE_NAME:-zenika/alpine-chrome:with-selenoid}
+export IMAGE_NAME
+
 DIR=$(cd $(dirname $0) && pwd)
 ${DIR}/../test.sh
-docker container run --rm --entrypoint '' ${IMAGE_NAME:-zenika/alpine-chrome:with-selenoid} selenoid -version
+
+echo "# Selenoid test"
+docker container run --rm --entrypoint '' ${IMAGE_NAME} selenoid -version
